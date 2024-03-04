9eedbba379031020af85be08b46a8b56def729ed
jkl <alexwang925@gmail.com>
2024-03-04 15:36:09 -0400

    final for reals

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 // Declared the variables needed for this exercise; both numbers and strings.
 const combo1 = 5 + 5;
 const combo2 = 4 * 10;
-const combo3 = 239 % 2;
+const combo3 = 239 % 200;
 
 const messageStr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
 // Realized I needed dashes between the codes, so added another string.
