23f5773e984c6ac7f4a41464f50e2819d5ca4a0d
jkl <alexwang925@gmail.com>
2024-03-04 17:04:40 -0400

    final final

--- main.js
+++ main.js
@@ -13,7 +13,6 @@ const combo3 = 239 % 200;
 const messageStr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
 // Realized I needed dashes between the codes, so added another string.
 const dashStr = " - ";
-101
 // Used 'alert' method for the dialog box pop-up.
 alert(messageStr + combo1 + dashStr + combo2 + dashStr + combo3);
 
