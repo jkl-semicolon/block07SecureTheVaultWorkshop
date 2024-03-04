b600ba3cbc40937be6033ac565ba80841f532f29
jkl <alexwang925@gmail.com>
2024-03-04 15:48:42 -0400

    bonus exercise added

--- main.js
+++ main.js
@@ -13,6 +13,13 @@ const combo3 = 239 % 200;
 const messageStr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
 // Realized I needed dashes between the codes, so added another string.
 const dashStr = " - ";
-
+101
 // Used 'alert' method for the dialog box pop-up.
 alert(messageStr + combo1 + dashStr + combo2 + dashStr + combo3);
+
+// Bonus method for the user to enter the secret combination is below:
+const userEnter1 = prompt("Please enter the first combination number: ");
+const userEnter2 = prompt("Please enter the second combination number: ");
+const userEnter3 = prompt("Please enter the third combination number: ");
+
+alert(messageStr + userEnter1 + dashStr + userEnter2 + dashStr + userEnter3);
