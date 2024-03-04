8e4785bccc2a3eedab02130019558655c8955cc5
jkl <alexwang925@gmail.com>
2024-03-04 15:31:56 -0400

    final

--- main.js
+++ main.js
@@ -5,11 +5,14 @@
 4. Add comments for explanations of code where necessary.
 */
 
+// Declared the variables needed for this exercise; both numbers and strings.
 const combo1 = 5 + 5;
 const combo2 = 4 * 10;
 const combo3 = 100 - 61;
 
 const messageStr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+// Realized I needed dashes between the codes, so added another string.
 const dashStr = " - ";
 
+// Used 'alert' method for the dialog box pop-up.
 alert(messageStr + combo1 + dashStr + combo2 + dashStr + combo3);
