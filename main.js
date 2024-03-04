cfde8d80ccebe6368372a4c8ca0837fcdb01b222
jkl <alexwang925@gmail.com>
2024-03-04 15:30:12 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/*  Pseudocode
+1. Define the variables needed for this assignment.
+2. Assign the variables with the necessary arithmetic operators.
+3. Create the dialog box using the 'alert' method.
+4. Add comments for explanations of code where necessary.
+*/
+
+const combo1 = 5 + 5;
+const combo2 = 4 * 10;
+const combo3 = 100 - 61;
+
+const messageStr = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+const dashStr = " - ";
+
+alert(messageStr + combo1 + dashStr + combo2 + dashStr + combo3);
